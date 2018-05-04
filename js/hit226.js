3c80c7abee2e423709dd893f698353599ceccf26
Ash0011 <mycdu.edu@icloud.com>
2018-05-04 12:40:14 +0930

    cASe correction for CSS & css 2

--- js/hit226.js
+++ js/hit226.js
@@ -1,7 +1,7 @@
 $(function(){
   $('ul.parent > li').hover(function(){
-    $(this).find('ul.child').show(300);
+    $(this).find('ul.child').show(600);
   }, function(){
-    $(this).find('ul.child').hide(300);
+    $(this).find('ul.child').hide(600);
   });
 });
